d090dc6b2d43696ab36a2347ca3fbc88a0f7a303
Dmytro-Ivanenko <dmitro0399@gmail.com>
2022-12-11 13:35:20 +0200

    Update search.js

--- src/js/search.js
+++ src/js/search.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
-export default async function search(photoName) {
-  const response = await axios.get(
-    `https://pixabay.com/api/?key=31970293-19f969d9a323cf342718bb6ce&q=${photoName.replace(
-      ' ',
-      '+'
-    )}&image_type=photo`
-  );
-  console.log(response);
-  return response;
+export default class PixabayAPI {
+  constructor() {
+    this.page = 1;
+    this.perPage = 40;
+    this.totalHints = 0;
+  }
+
+  async search(photoName) {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=31970293-19f969d9a323cf342718bb6ce&q=${photoName.replace(
+        ' ',
+        '+'
+      )}&image_type=photo&orientation=horizontal&safesearch=true&page=${
+        this.page
+      }&per_page=${this.perPage}`
+    );
+    console.log(response);
+    return response;
+  }
 }
